2d4b9b240cdce219343815deccce59ab24278cee
Cedric Hornberger <ceddi.hornberger@gmx.de>
2026-06-08 16:25:37 +0200

    Refactor canvas rotation logic and update image descriptor initialization

--- boards/shields/nice_view_custom/widgets/util.c
+++ boards/shields/nice_view_custom/widgets/util.c
@@ -13,15 +13,17 @@ LV_IMG_DECLARE(bolt);
 void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
     static lv_color_t cbuf_tmp[CANVAS_SIZE * CANVAS_SIZE];
     memcpy(cbuf_tmp, cbuf, sizeof(cbuf_tmp));
-    lv_img_dsc_t img;
-    img.data = (void *)cbuf_tmp;
-    img.header.cf = LV_IMG_CF_TRUE_COLOR;
+    lv_img_dsc_t img = {0};
+    img.data = (const uint8_t *)cbuf_tmp;
+    img.data_size = sizeof(cbuf_tmp);
+    img.header.cf = LV_COLOR_FORMAT_NATIVE;
     img.header.w = CANVAS_SIZE;
     img.header.h = CANVAS_SIZE;
+    img.header.stride = CANVAS_SIZE * sizeof(lv_color_t);
 
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, CANVAS_SIZE / 2,
-                        CANVAS_SIZE / 2, true);
+    lv_canvas_transform(canvas, &img, 900, 256, 256, -1, 0, CANVAS_SIZE / 2,
+                        CANVAS_SIZE / 2);
 }
 
 void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
