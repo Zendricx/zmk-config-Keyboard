22c18a7d13cc9ae0c79bb8fa38a0fbb171260595
Cedric Hornberger <ceddi.hornberger@gmx.de>
2026-07-30 21:03:11 +0200

    try if rotate is broken

--- boards/shields/nice_view_custom/widgets/util.c
+++ boards/shields/nice_view_custom/widgets/util.c
@@ -11,13 +11,9 @@
 LV_IMG_DECLARE(bolt);
 
 void rotate_canvas(lv_obj_t *canvas) {
-    uint8_t *buf = lv_canvas_get_draw_buf(canvas)->data;
-    static uint8_t buf_copy[CANVAS_BUF_SIZE];
-    memcpy(buf_copy, buf, sizeof(buf_copy));
-
-    const uint32_t stride = lv_draw_buf_width_to_stride(CANVAS_SIZE, CANVAS_COLOR_FORMAT);
-    lv_draw_sw_rotate(buf_copy, buf, CANVAS_SIZE, CANVAS_SIZE, stride, stride,
-                      LV_DISPLAY_ROTATION_270, CANVAS_COLOR_FORMAT);
+    ARG_UNUSED(canvas);
+    // Temporarily disabled to isolate a crash - see debugging session.
+    // Display will render unrotated while this is stubbed out.
 }
 
 void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
